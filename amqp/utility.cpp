a8d118dff6b4f1c52e26eda51c0605264a29efe7
Urs Fässler <urs.fassler@bbv.ch>
2019-05-31 10:20:58 +0200

    more detailed error message on server connection error

--- amqp/utility.cpp
+++ amqp/utility.cpp
@@ -10,9 +10,37 @@
 #include <amqp_tcp_socket.h>
 
 #include <stdexcept>
+#include <map>
+
 
 namespace amqp
 {
+namespace
+{
+
+
+const std::map<amqp_method_number_t, std::string> MethodNames {
+  {AMQP_CONNECTION_CLOSE_METHOD, "connection"},
+  {AMQP_CHANNEL_CLOSE_METHOD, "channel"},
+};
+
+std::string serverExceptionMessage(const amqp_method_t& error)
+{
+  const auto id = error.id;
+  const auto idx = MethodNames.find(id);
+  if (idx == MethodNames.end()) {
+    std::string fe = "unknown server error, method id " + std::to_string(id);
+    return fe;
+  } else {
+    amqp_connection_close_t *m = static_cast<amqp_connection_close_t*>(error.decoded);
+    std::string em{static_cast<const char *>(m->reply_text.bytes), m->reply_text.len};
+    std::string fe = "server " + idx->second + " error " + std::to_string(m->reply_code) +  "; " + em;
+    return fe;
+  }
+}
+
+
+}
 
 
 void fail_if(bool predicate, const std::string &message)
@@ -22,8 +50,13 @@ void fail_if(bool predicate, const std::string &message)
   }
 }
 
-void fail_if_error(const amqp_rpc_reply_t &reply, const std::string &message)
+void fail_if_error(const amqp_rpc_reply_t& reply, const std::string &message)
 {
+  if (reply.reply_type == AMQP_RESPONSE_SERVER_EXCEPTION) {
+    const amqp_method_t& mr = reply.reply;
+    const std::string fe = message + ": " + serverExceptionMessage(mr);
+    throw std::runtime_error(fe);
+  }
   fail_if(reply.reply_type != AMQP_RESPONSE_NORMAL, message);
 }
 
